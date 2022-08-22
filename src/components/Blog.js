7e4b757599e7c7f57260b274911600158e8bb7c2
LukasD <lukas.dvarionis@gmail.com>
2022-08-22 15:53:29 +0300

    5.7* Blog list frontend, step7 Added border styles for Blog component Added view button to unfold Blog details and hide button to hide details

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,29 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  console.log(blog);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const [visibleDetails, setVisibleDetails] = useState(false);
+  const toggleVisibility = () => {
+    setVisibleDetails(!visibleDetails);
+  }
+
+  return (
+  <div style = {blogStyle}>
+    <div>{blog.title} {blog.author} <button onClick={toggleVisibility}>{visibleDetails ? 'hide' : 'view'}</button> </div>
+    <div style={ {display: visibleDetails? '' : 'none'} }>
+      <div>{blog.url}</div>
+      <div>likes {blog.likes} <button>like</button> </div>
+      <div>{blog.user ? blog.user.name : ''}</div>
+    </div>
+  </div> 
+  )
+}
 
 export default Blog

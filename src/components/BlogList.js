c181031ee67659740f93a034ec8eac54dc2a139c
LukasD <lukas.dvarionis@gmail.com>
2022-08-22 19:02:32 +0300

    5.9: Blog list frontend, step9 Added Desc sort by likes to blog list

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -6,10 +6,12 @@ const BlogList = ({blogs, user, setUser, setBlogs}) => {
         const updatedBlogList = blogs.map(blog => { return blog.id == updatedBlog.id ? updatedBlog : blog })
         setBlogs(updatedBlogList);
     }
-    
+
     return (
         <div>
-            {blogs.map(blog =>
+            {blogs
+            .sort((a, b) => b.likes - a.likes)
+            .map(blog =>
             <Blog key={blog.id} blog={blog} updateBlogList={updateBlogList} />
             )}
       </div>
